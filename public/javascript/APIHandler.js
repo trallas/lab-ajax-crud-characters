8605fa6799b8f67c4ec5d045dcc0ad285c5d9018
Xavi <xaviertralleroguillot@gmail.com>
2023-05-11 17:13:28 +0200

    lab solved

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,11 +4,29 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return this.api.get('/characters')
+      .then(response => {
+        console.log ('Retrieveing all the characters');
+        console.log('response data'), response.data);
+        return response.data
+      })
+    .catch (()) => {
+      console.log('Something went wrong, there are no characters');
+      return 'Something went wrong, there are no characters';
+    })
   }
 
   getOneRegister () {
-
+    return this.api.get ('/characters/:')
+    .then(response => {
+      console.log('Retrieving one character')
+      console.log('response data', response.data)
+      return response.data
+    })
+    .catch (()) => {
+      console.log('Something went wrong, there are no characters');
+      return 'Something went wrong, there are no characters';
+    }
   }
 
   createOneRegister () {
